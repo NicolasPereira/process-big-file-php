b29fb9eb8660f49923e91a55d65974be6161f68c
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-06-19 04:18:40 -0300

    feat: adicionando info de memoria

--- src/Fibers/fibers.php
+++ src/Fibers/fibers.php
@@ -13,3 +13,9 @@ echo $fiber->start();
 while ($item = $fiber->resume()) {
     printf('%s tem %d anos%s', $item[0], $item[1], PHP_EOL);
 }
+
+$memoria = round(memory_get_peak_usage() / 1024, 0);
+$tamanho =round(filesize('../file.csv') / 1024 / 1024, 0);
+
+echo "Memória utilizada: " . $memoria . "KB" . PHP_EOL;
+echo "Tamanho do arquivo: " . $tamanho . "MB" . PHP_EOL;
--- src/Generator/generators.php
+++ src/Generator/generators.php
@@ -10,3 +10,9 @@ $conteudo = $leitor->lerArquivo();
 foreach ($conteudo as $item) {
         printf('%s tem %d anos%s', $item[0], $item[1], PHP_EOL);
 }
+
+$memoria = round(memory_get_peak_usage() / 1024, 0);
+$tamanho =round(filesize('../file.csv') / 1024 / 1024, 0);
+
+echo "Memória utilizada: " . $memoria . "KB" . PHP_EOL;
+echo "Tamanho do arquivo: " . $tamanho . "MB" . PHP_EOL;
